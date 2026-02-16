b6b77ddd8c651d1b695ff80028590372f18717e4
albor68 <45852729+albor68@users.noreply.github.com>
2026-02-16 11:22:52 +0300

    Добавлен текст miniblink

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 
 int main () {
 
+     rcc_periph_clock_enable(RCC_GPIOD);
+     gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
+     
      while (true) {
 
-
+          gpio_toggle(GPIOD, GPIO15);
+		for (volatile uint32_t i = 0; i < 1000000; i++);
 
      }
 
